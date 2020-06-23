85e01d20daf0d7fc51e4227ccc31967c6f15a7b5
Marcus Menezes <marcus.menezes@kuehne-nagel.com>
2020-06-23 11:14:15 +0300

    Creating State Management - PROVIDER

---
+++ lib/widgets/products_grid.dart
@@ -0,0 +1,32 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_app/providers/products_provider.dart';
+import 'package:shop_app/widgets/product_item.dart';
+
+class ProductsGrid extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    //adding this class as listener from provider
+    final productsData = Provider.of<Products>(context);
+    final loadedProducts = productsData.items;
+    return GridView.builder(
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3/2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10
+      ),
+      itemBuilder: (ctx, index) => ProductItem(
+        loadedProducts[index].id,
+        loadedProducts[index].title,
+        loadedProducts[index].imageUrl,
+      ),
+      itemCount: loadedProducts.length,
+      padding: const EdgeInsets.all(10.0),
+    );
+  }
+}
+
