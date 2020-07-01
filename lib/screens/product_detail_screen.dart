74a0ce318aa7a5450b9bb8d538a39de6caf2575c
Marcus Menezes <marcus.menezes@kuehne-nagel.com>
2020-07-01 08:06:43 +0300

    Detail screen

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:shop_app/providers/products_provider.dart';
+import 'package:shop_app/providers/products.dart';
 
 class ProductDetailScreen extends StatelessWidget {
  /* final String title;*/
@@ -16,6 +16,23 @@ class ProductDetailScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text(loadedProduct.title),
+
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              height: 300,
+              child: Image.network(loadedProduct.imageUrl, fit: BoxFit.cover,),
+              width: double.infinity,
+            ),
+            SizedBox(height: 10,),
+            Text('\$${loadedProduct.price}', style: TextStyle(color: Colors.grey, fontSize: 20),),
+            SizedBox(height: 10,),
+            Text(loadedProduct.description, textAlign: TextAlign.center, softWrap: true,)
+          ],
+        ),
       ),
     );
   }
