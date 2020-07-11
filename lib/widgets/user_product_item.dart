44b72ec3f2b2e75270564839f4c43f520fe025e9
Marcus Menezes <marcus.menezes@kuehne-nagel.com>
2020-07-11 14:34:15 +0200

    Delete product

--- lib/widgets/user_product_item.dart
+++ lib/widgets/user_product_item.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_app/providers/products.dart';
 import 'package:shop_app/screens/edit_product_screen.dart';
 
 class UserProductItem extends StatelessWidget {
@@ -18,9 +20,11 @@ class UserProductItem extends StatelessWidget {
         width: 100,
         child: Row(children: <Widget>[
           IconButton(icon: Icon(Icons.edit), onPressed: () {
-            Navigator.of(context).pushNamed(EditProductScreen.routeName, arguments: this.id );
+            Navigator.of(context).pushNamed(EditProductScreen.routeName, arguments: id );
           },color: Theme.of(context).primaryColor,),
-          IconButton(icon: Icon(Icons.delete), onPressed: () {},color: Theme.of(context).errorColor,),
+          IconButton(icon: Icon(Icons.delete), onPressed: () {
+            Provider.of<Products>(context, listen: false).deleteProduct(id);
+          },color: Theme.of(context).errorColor,),
         ],),
       ),
     );
