a298c16759b1910331ce99c2253a6ba99eac514f
Marcus Menezes <marcus.menezes@kuehne-nagel.com>
2020-07-14 15:27:07 +0300

    Logout and Logout Timer

--- lib/widgets/app_drawer.dart
+++ lib/widgets/app_drawer.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_app/providers/auth.dart';
 import 'package:shop_app/screens/order_screen.dart';
 import 'package:shop_app/screens/user_products_screen.dart';
 
@@ -34,6 +36,17 @@ class AppDrawer extends StatelessWidget {
             title: Text('Manage Products'),
             onTap: () {
               Navigator.of(context).pushReplacementNamed(UserProductsScreen.routeName);
+            },
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.exit_to_app),
+            title: Text('Logout'),
+            onTap: () {
+              //close the drawer
+              Navigator.of(context).pop();
+
+              Provider.of<Auth>(context, listen: false).logout();
             },
           )
         ],
