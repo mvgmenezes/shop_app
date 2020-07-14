3adf4bd25b6865fbf03eb1a509a7fc681a0444cd
Marcus Menezes <marcus.menezes@kuehne-nagel.com>
2020-07-14 12:27:15 +0300

    Authentication - Sign in Sign up and error

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -2,19 +2,38 @@ import 'dart:convert';
 
 import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
+import 'package:shop_app/models/http_exception.dart';
 
 class Auth with ChangeNotifier {
   String _token;
   DateTime _expiryDate;
   String _userId;
 
+  Future<void> _authenticated(String email, String password, String urlSegment) async {
+    final url = "https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=AIzaSyDek6P-sR2YKPdj4viM6DrhcEeCurYLUCo";
+    try{
+      final response = await http.post(url, body: json.encode({
+        'email': email,
+        'password': password,
+        'returnSecureToken': true
+      }));
+      final responseData = json.decode(response.body);
+      if(responseData['error'] != null){
+        throw HttpException(responseData['error']['message']);
+      }
+      print(response.body);
+    }catch(error){
+      throw error;
+    }
+
+  }
+
+
   Future<void> signup(String email, String password) async {
-    final url = "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDek6P-sR2YKPdj4viM6DrhcEeCurYLUCo";
-    final response = await http.post(url, body: json.encode({
-      'email': email,
-      'password': password,
-      'returnSecureToken': true
-    }));
-    print(response.body);
+    return _authenticated(email, password, "signUp");
+  }
+
+  Future<void> signin(String email, String password) async {
+    return _authenticated(email, password, "signInWithPassword");
   }
 }
--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -2,6 +2,7 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shop_app/models/http_exception.dart';
 import 'package:shop_app/providers/auth.dart';
 
 enum AuthMode { Signup, Login }
@@ -102,7 +103,21 @@ class _AuthCardState extends State<AuthCard> {
   var _isLoading = false;
   final _passwordController = TextEditingController();
 
-  void _submit() {
+  void _showErrorDialog(String message){
+    showDialog(
+      context: context,
+      builder: (ctx) => AlertDialog(
+        title: Text('An error occurred!'),
+        content: Text(message),
+        actions: <Widget>[
+          FlatButton(child: Text('Okay'),onPressed: () {
+            Navigator.of(ctx).pop();
+          },)
+        ],
+      )
+    );
+  }
+  Future<void> _submit() async {
     if (!_formKey.currentState.validate()) {
       // Invalid!
       return;
@@ -111,11 +126,31 @@ class _AuthCardState extends State<AuthCard> {
     setState(() {
       _isLoading = true;
     });
-    if (_authMode == AuthMode.Login) {
-      // Log user in
-    } else {
-      // Sign user up
-      Provider.of<Auth>(context, listen: false).signup(_authData['email'], _authData['password']);
+    try{
+      if (_authMode == AuthMode.Login) {
+        // Log user in
+        await Provider.of<Auth>(context, listen: false).signin(_authData['email'], _authData['password']);
+      } else {
+        // Sign user up
+        await Provider.of<Auth>(context, listen: false).signup(_authData['email'], _authData['password']);
+      }
+    }on HttpException catch(error){
+      var errorMessage = 'Authenticate failed';
+      if(error.toString().contains('EMAIL_EXISTS')){
+        errorMessage = 'This email address is already in use';
+      }else if(error.toString().contains('INVALID_EMAIL')){
+        errorMessage = 'This is not a valid email address';
+      }else if(error.toString().contains('WEAK_PASSWORD')){
+        errorMessage = 'This password is too weak';
+      }else if(error.toString().contains('EMAIL_NOT_FOUND')){
+        errorMessage = 'Could not find a user with that email';
+      }else if(error.toString().contains('INVALID_PASSWORD')){
+        errorMessage = 'This password is invalid';
+      }
+      _showErrorDialog(errorMessage);
+    }catch(error){
+      const errorMessage = 'Could not authenticate you. please try again.';
+      _showErrorDialog(errorMessage);
     }
     setState(() {
       _isLoading = false;
